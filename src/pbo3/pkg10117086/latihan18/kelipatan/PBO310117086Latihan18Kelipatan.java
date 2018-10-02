1afe359b01fa5e2facaa5bfd01c843e642dc0a97
djulfikar <muhammaddjulfikar7@gmail.com>
2018-10-01 21:52:45 -0700

    PBO3-10117086-Latihan18-Kelipatan

---
+++ src/pbo3/pkg10117086/latihan18/kelipatan/PBO310117086Latihan18Kelipatan.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117086.latihan18.kelipatan;
+
+/**
+ *
+ * @author Lenovo NAMA : RD Muhammad Djulfikar BU
+ * KELAS : IF3 
+ * NIM : 10117086
+ * DESKRIPSI PROGRAM : Membuat program untuk kelipatan
+ */
+public class PBO310117086Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (double i = 3.5; i <= 35; i = i + 3.5) {
+            System.out.println(i);
+        }
+    }
+}
